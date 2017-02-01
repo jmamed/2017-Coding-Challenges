4015c41dffd62dc5627f11199548fe7fbfd9ced3
Jason Mamed <mamed.jason@gmail.com>
2017-02-01 14:33:02 -0600

    HackerRank Sum of 3 Integers
    This c++ file takes 3 integer inputs and outputs the sum of the inputs.
    Challenge given from Hackerrank email.

---
+++ HackerrankCpluspluschallenge.c
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+#In this challenge, we're practicing reading input from stdin and printing output to stdout.
+#Read 3 numbers from stdin and print their sum to stdout.
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int a;
+    int b;
+    int c;
+    
+    cin >> a ;
+    cin >> b ;
+    cin >> c ;
+    
+    cout << a+b+c << endl;
+    return 0;
+}
